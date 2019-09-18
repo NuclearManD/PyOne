69bd99096ef45d095874494b91f4cf6e37c98b52
NuclearManD <dylanbrophy@gmail.com>
2019-09-17 19:16:06 -0700

    Package...

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nuclaer-pyone",
+    version="0.0.0",
+    author="Dylan Brophy",
+    author_email="dylanbrophy@gmail.com",
+    description="Decentralized data storage framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NuclearManD/PyOne",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'ecdsa', 'ssl'
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
